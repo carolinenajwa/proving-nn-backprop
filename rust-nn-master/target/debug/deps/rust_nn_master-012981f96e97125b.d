9f60c87c2a4b7bcea2132d10251eb40a4532bdd0
Caroline E <82617020+carolinenajwa@users.noreply.github.com>
2024-01-20 22:06:09 -0800

    new cargo

---
+++ rust-nn-master/target/debug/deps/rust_nn_master-012981f96e97125b.d
@@ -0,0 +1,5 @@
+/Users/caroline/Documents/GitHub/proving-nn-backprop/rust-nn-master/target/debug/deps/librust_nn_master-012981f96e97125b.rmeta: src/main.rs
+
+/Users/caroline/Documents/GitHub/proving-nn-backprop/rust-nn-master/target/debug/deps/rust_nn_master-012981f96e97125b.d: src/main.rs
+
+src/main.rs:
---
+++ rust-nn-master/target/debug/deps/rust_nn_master-3d4f6ee75b8e0c54.d
@@ -0,0 +1,5 @@
+/Users/caroline/Documents/GitHub/proving-nn-backprop/rust-nn-master/target/debug/deps/librust_nn_master-3d4f6ee75b8e0c54.rmeta: src/main.rs
+
+/Users/caroline/Documents/GitHub/proving-nn-backprop/rust-nn-master/target/debug/deps/rust_nn_master-3d4f6ee75b8e0c54.d: src/main.rs
+
+src/main.rs:
