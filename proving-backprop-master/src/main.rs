80503ca46030590dceb5bc68f857fc8d1fe5f3b3
carolinenajwa <82617020+carolinenajwa@users.noreply.github.com>
2024-01-20 21:57:02 -0800

    Delete proving-backprop-master/src directory

--- proving-backprop-master/src/main.rs
+++
@@ -1,9 +0,0 @@
-
-// Import the ArithmFunctions module
-mod arithm_functions;
-use arithm_functions::ArithmeticFunctions;
-
-pub fn main() {
-    ArithmeticFunctions::run_dot_product_test();
-    
-}
